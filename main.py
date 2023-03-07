2cd91d65d4593263abb4f20342709562a00066fe
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-03-07 15:01:53 -0300

    Clean up

--- main.py
+++ main.py
@@ -272,9 +272,6 @@ def prompt(update: Update, context: CallbackContext) -> None:
         .text[:MAX_MESSAGE_LENGTH]
     )
 
-def crash(update: Update, context: CallbackContext) -> None:
-    raise TypeError()
-
 
 def error_handler(update: object, context: CallbackContext) -> None:
     if not isinstance(update, Update):
@@ -305,7 +302,6 @@ dispatcher.add_handler(CommandHandler("rules", rules))
 dispatcher.add_handler(CommandHandler("slap", slap))
 dispatcher.add_handler(CommandHandler("vagabundo", tramp))
 dispatcher.add_handler(CommandHandler("prompt", prompt))
-dispatcher.add_handler(CommandHandler("crash", crash))
 dispatcher.add_error_handler(error_handler)
 
 
