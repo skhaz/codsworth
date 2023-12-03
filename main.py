6e66f46227c0ca4f1f9a462cdd00fd317ff3c622
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-12-03 18:11:30 -0300

    Work in progress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import functools
 import io
 import mimetypes
-import os
 import re
 import subprocess
 import unicodedata
@@ -10,7 +9,6 @@ from html import escape
 from pathlib import Path
 from random import choice
 from random import random
-from tempfile import TemporaryDirectory
 from typing import Union
 
 import jinja2
@@ -520,7 +518,50 @@ def index() -> Response:
         sync_playwright() as playwright,
     ):
         chromium = playwright.chromium
-        browser = chromium.launch()
+
+        browser = chromium.launch(
+            headless=True,
+            args=[
+                "--autoplay-policy=user-gesture-required",
+                "--disable-background-networking",
+                "--disable-background-timer-throttling",
+                "--disable-backgrounding-occluded-windows",
+                "--disable-breakpad",
+                "--disable-client-side-phishing-detection",
+                "--disable-component-update",
+                "--disable-default-apps",
+                "--disable-dev-shm-usage",
+                "--disable-domain-reliability",
+                "--disable-extensions",
+                "--disable-features=AudioServiceOutOfProcess",
+                "--disable-hang-monitor",
+                "--disable-ipc-flooding-protection",
+                "--disable-notifications",
+                "--disable-offer-store-unmasked-wallet-cards",
+                "--disable-popup-blocking",
+                "--disable-print-preview",
+                "--disable-prompt-on-repost",
+                "--disable-renderer-backgrounding",
+                "--disable-setuid-sandbox",
+                "--disable-speech-api",
+                "--disable-sync",
+                "--disk-cache-size=33554432",
+                "--hide-scrollbars",
+                "--ignore-gpu-blacklist",
+                "--metrics-recording-only",
+                "--mute-audio",
+                "--no-default-browser-check",
+                "--no-first-run",
+                "--no-pings",
+                "--no-sandbox",
+                "--no-zygote",
+                "--password-store=basic",
+                "--use-gl=swiftshader",
+                "--use-mock-keychain",
+                "--single-process",
+            ],
+        )
+
         context = browser.new_context(
             viewport={
                 "width": 1600,
