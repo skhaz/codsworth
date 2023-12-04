35e9ae78d1c4a8e4cff82a463eeb298a9a45d3ac
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-12-04 07:55:46 -0300

    Work in progress

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import functools
 import io
 import mimetypes
+import os
 import re
 import subprocess
 import unicodedata
 from functools import wraps
 from html import escape
+from http import HTTPStatus
 from pathlib import Path
+from queue import Queue
 from random import choice
 from random import random
 from typing import Union
@@ -15,33 +18,43 @@ import jinja2
 import numpy as np
 import openai
 import PIL.Image
+import sentry_sdk
 import yaml
 from flask import Flask
-from flask import send_file
+from flask import make_response
+from flask import request
 from fuzzywuzzy import fuzz
 from google.cloud.vision import Image
+from google.cloud.vision import ImageAnnotatorClient
 from playwright.sync_api import sync_playwright
+from redis import ConnectionPool
+from redis import Redis
 from redis_rate_limit import RateLimit
 from redis_rate_limit import TooManyRequests
+from telegram import Bot
 from telegram import ChatAction
 from telegram import ParseMode
 from telegram import Update
 from telegram.error import TelegramError
 from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import Dispatcher
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
 from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
-# vision = vision.ImageAnnotatorClient()
+vision = ImageAnnotatorClient()
 
-# redis_pool = ConnectionPool.from_url(os.environ["REDIS_DSN"])
-# redis = Redis(connection_pool=redis_pool)
+redis_pool = ConnectionPool.from_url(os.environ["REDIS_DSN"])
+redis = Redis(connection_pool=redis_pool)
 
-# openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# mimetypes.init()
+mimetypes.init()
 
-# sentry_sdk.init(dsn=os.environ["SENTRY_DSN"])
+sentry_sdk.init(dsn=os.environ["SENTRY_DSN"])
 
 with open("memes.yaml", "rt", encoding="utf-8") as f:
     memes = yaml.safe_load(f)
@@ -390,50 +403,6 @@ def image(update: Update, context: CallbackContext) -> None:
         pass
 
 
-# def trim(buffer, margin=32, trim_color="#92b88a"):
-#     with PIL.Image.open(io.BytesIO(buffer)).convert("RGB") as image:
-#         width, height = image.size
-
-#         left, top, right, bottom = 0, 0, width - 1, height - 1
-
-#         trim_color_rgb = tuple(int(trim_color[i : i + 2], 16) for i in (1, 3, 5))
-
-#         while left < width:
-#             column = [image.getpixel((left, y)) for y in range(height)]
-#             if all(pix == trim_color_rgb for pix in column):
-#                 left += 1
-#             else:
-#                 break
-
-#         while top < height:
-#             row = [image.getpixel((x, top)) for x in range(width)]
-#             if all(pix == trim_color_rgb for pix in row):
-#                 top += 1
-#             else:
-#                 break
-
-#         while right > left:
-#             column = [image.getpixel((right, y)) for y in range(height)]
-#             if all(pix == trim_color_rgb for pix in column):
-#                 right -= 1
-#             else:
-#                 break
-
-#         while bottom > top:
-#             row = [image.getpixel((x, bottom)) for x in range(width)]
-#             if all(pix == trim_color_rgb for pix in row):
-#                 bottom -= 1
-#             else:
-#                 break
-
-#         left = max(left - margin, 0)
-#         top = max(top - margin, 0)
-#         right = min(right + margin, width - 1)
-#         bottom = min(bottom + margin, height - 1)
-
-#         return image.crop((left, top, right, bottom))
-
-
 def trim(buffer, margin=32, trim_color="#92b88a"):
     with PIL.Image.open(io.BytesIO(buffer)).convert("RGB") as image:
         data = np.array(image)
@@ -457,79 +426,6 @@ def ditto(update: Update, context: CallbackContext) -> None:
     if not message:
         return
 
-
-def error_handler(update: object, context: CallbackContext) -> None:
-    if not isinstance(update, Update):
-        return
-
-    error = context.error
-
-    if not error:
-        return
-
-    message = update.effective_message
-
-    if not message:
-        return
-
-    mention = mention_html(user_id=message.from_user.id, name=message.from_user.name)
-    caption = f"{mention} me causou câncer 💀"
-    filename = choice(list(Path("assets/died").iterdir()))
-    with open(filename, "rb") as f:
-        context.bot.send_photo(
-            message.chat_id,
-            caption=caption,
-            photo=f,
-            parse_mode=ParseMode.HTML,
-        )
-
-    raise error
-
-
-# bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
-
-# """
-# fortune - tell someone's fortune
-# repost - tell them that post already have been posted
-# rules - tell the rules
-# slap - slaps someone else
-# vagabundo - tell the person is lazy
-# ban - bans someone else
-# leaderboard - featured users
-# reply - reply to a message using ChatGPT
-# prompt - generate a text using AI
-# image - generate a image using AI
-# ditto - copy someone else's message
-# """
-# dispatcher = Dispatcher(bot=bot, update_queue=Queue())
-# dispatcher.add_handler(MessageHandler(Filters.regex(r"^s/"), sed))
-# dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, meme))
-# dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, on_enter))
-# dispatcher.add_handler(CommandHandler("fortune", fortune))
-# dispatcher.add_handler(CommandHandler("repost", repost))
-# dispatcher.add_handler(CommandHandler("rules", rules))
-# dispatcher.add_handler(CommandHandler("slap", slap))
-# dispatcher.add_handler(CommandHandler("vagabundo", tramp))
-# dispatcher.add_handler(CommandHandler("ban", ban))
-# dispatcher.add_handler(CommandHandler("reply", reply))
-# dispatcher.add_handler(CommandHandler("prompt", prompt))
-# dispatcher.add_handler(CommandHandler("image", image))
-# dispatcher.add_handler(CommandHandler("ditto", ditto))
-# dispatcher.add_error_handler(error_handler)
-
-
-# @app.post("/")
-# def index() -> Response:
-#     dispatcher.process_update(Update.de_json(request.get_json(force=True), bot))
-
-#     response = make_response()
-#     response.status_code = HTTPStatus.NO_CONTENT
-#     response.mimetype = "application/json"
-#     return response
-
-
-@app.get("/get")
-def index() -> Response:
     with (
         open("assets/ditto/index.html", "rt") as html,
         sync_playwright() as playwright,
@@ -579,17 +475,15 @@ def index() -> Response:
             ],
         )
 
-        context = browser.new_context(
+        page = browser.new_context(
             viewport={
                 "width": 1600,
                 "height": 1200,
             },
             device_scale_factor=2,
-        )
+        ).new_page()
 
-        page = context.new_page()
-
-        context = {
+        c = {
             "messages": [
                 {
                     "avatar": "https://github.com/skhaz.png",
@@ -608,7 +502,7 @@ def index() -> Response:
 
         environment = jinja2.Environment()
         template = environment.from_string(html.read())
-        page.set_content(template.render(context))
+        page.set_content(template.render(c))
 
         data = trim(page.screenshot())
 
@@ -616,9 +510,74 @@ def index() -> Response:
         data.save(buffer, format="PNG")
         data = buffer.getvalue()
 
-        return send_file(
-            io.BytesIO(data),
-            download_name="logo.png",
-            mimetype="image/png",
+        message.reply_photo(data)
+
+
+def error_handler(update: object, context: CallbackContext) -> None:
+    if not isinstance(update, Update):
+        return
+
+    error = context.error
+
+    if not error:
+        return
+
+    message = update.effective_message
+
+    if not message:
+        return
+
+    mention = mention_html(user_id=message.from_user.id, name=message.from_user.name)
+    caption = f"{mention} me causou câncer 💀"
+    filename = choice(list(Path("assets/died").iterdir()))
+    with open(filename, "rb") as f:
+        context.bot.send_photo(
+            message.chat_id,
+            caption=caption,
+            photo=f,
+            parse_mode=ParseMode.HTML,
         )
-        # return Response("ok")
+
+    raise error
+
+
+bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
+
+"""
+fortune - tell someone's fortune
+repost - tell them that post already have been posted
+rules - tell the rules
+slap - slaps someone else
+vagabundo - tell the person is lazy
+ban - bans someone else
+leaderboard - featured users
+reply - reply to a message using ChatGPT
+prompt - generate a text using AI
+image - generate a image using AI
+ditto - copy someone else's message
+"""
+dispatcher = Dispatcher(bot=bot, update_queue=Queue())
+dispatcher.add_handler(MessageHandler(Filters.regex(r"^s/"), sed))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, meme))
+dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, on_enter))
+dispatcher.add_handler(CommandHandler("fortune", fortune))
+dispatcher.add_handler(CommandHandler("repost", repost))
+dispatcher.add_handler(CommandHandler("rules", rules))
+dispatcher.add_handler(CommandHandler("slap", slap))
+dispatcher.add_handler(CommandHandler("vagabundo", tramp))
+dispatcher.add_handler(CommandHandler("ban", ban))
+dispatcher.add_handler(CommandHandler("reply", reply))
+dispatcher.add_handler(CommandHandler("prompt", prompt))
+dispatcher.add_handler(CommandHandler("image", image))
+dispatcher.add_handler(CommandHandler("ditto", ditto))
+dispatcher.add_error_handler(error_handler)
+
+
+@app.post("/")
+def index() -> Response:
+    dispatcher.process_update(Update.de_json(request.get_json(force=True), bot))
+
+    response = make_response()
+    response.status_code = HTTPStatus.NO_CONTENT
+    response.mimetype = "application/json"
+    return response
